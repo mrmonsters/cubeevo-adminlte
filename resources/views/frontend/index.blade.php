0be278fb889f3272837348363fc6cdc2074c56eb
Yc Ang <ycang@ozuraworld.com>
2015-08-27 15:51:26 +0800

    home ui fixes.

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,6 +1,7 @@
 @extends('partials.frontend.app')
 
 @section('frontend-content') 
+<div class="content-wrapper__fixed"></div>
 <div id="fullpage">
     @if (isset($content) && isset($content->content))
     	<?php echo html_entity_decode($content->content); ?>
