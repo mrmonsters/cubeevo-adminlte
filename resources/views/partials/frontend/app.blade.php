a26182eaabd779d2c39ef697a4237641d8c09fef
Yc Ang <ycang@ozuraworld.com>
2015-09-03 17:25:13 +0800

    ui fine tune.

--- resources/views/partials/frontend/app.blade.php
+++ resources/views/partials/frontend/app.blade.php
@@ -3,19 +3,20 @@
 	@include('partials.frontend.htmlheader')
 
 	<body>
-		<div class="blanket" style="position: fixed; top: 0px; left: 0px; width: 100%; height: 100%; z-index: 9999; background-color: rgb(0, 0, 0);">
+		<div class="blanket" style="position: fixed; top: 0px; left: 0px; width: 100%; height: 100%; z-index: 9999; background-color: #5c5756;">
 	    <div class="loader" style="position: absolute;
 		    top: 50%;
 		    left: 50%;
 		    width: 60px;
-		    height: 4px;
-		    background-color: #FFF;
+		    height: 4px; 
 		    margin-left: -30px;
 		    margin-top: -2px;
 		    animation: spin 1.5s infinite linear;
 		    animation-timing-function: ease;
 		    -webkit-animation: spin 1.5s infinite linear;
-		    -webkit-animation-timing-function: ease;"></div>
+		    -webkit-animation-timing-function: ease;">
+		    <img src="{{ url('/img/Programmer Needs-06.png')}}">
+		</div>
 		</div>
 		<!-- Navigation -->
 		@include('partials.frontend.menu')
