a8825c14c70914b8c77d4321a5993d4937191683
Yc Ang <ycang@ozuraworld.com>
2015-09-03 17:31:29 +0800

    update loader icon

--- resources/views/partials/frontend/app.blade.php
+++ resources/views/partials/frontend/app.blade.php
@@ -3,7 +3,7 @@
 	@include('partials.frontend.htmlheader')
 
 	<body>
-		<div class="blanket" style="position: fixed; top: 0px; left: 0px; width: 100%; height: 100%; z-index: 9999; background-color: #5c5756;">
+		<div class="blanket" style="position: fixed; top: 0px; left: 0px; width: 100%; height: 100%; z-index: 9999; background-color: #676464;">
 	    <div class="loader" style="position: absolute;
 		    top: 50%;
 		    left: 50%;
@@ -15,7 +15,7 @@
 		    animation-timing-function: ease;
 		    -webkit-animation: spin 1.5s infinite linear;
 		    -webkit-animation-timing-function: ease;">
-		    <img src="{{ url('/img/Programmer Needs-06.png')}}">
+		    <img src="{{ url('/img/LoaderIcon.png')}}" width="100%">
 		</div>
 		</div>
 		<!-- Navigation -->
