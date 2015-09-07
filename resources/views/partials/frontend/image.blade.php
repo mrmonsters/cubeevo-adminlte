60f26825711f1eab8589995a2d063760f8807133
Yc Ang <ycang@ozuraworld.com>
2015-09-07 14:21:31 +0800

    bug fix.

--- resources/views/partials/frontend/image.blade.php
+++ resources/views/partials/frontend/image.blade.php
@@ -3,8 +3,10 @@ use App\Models\Files;
 
 $imgIds = explode(",", $block->value);
 ?>
+@if($imgIds[0] != '')
 <div class="row"> 
     <div class="col-xs-12 nopadding">
         <img src="{{ Files::find($imgIds[0])->dir }}" width="100%">
     </div>
 </div>
+@endif
