50a9731def02e922326e7cb2bed407e0c126bacb
ycang89 <ycang@Ycs-MacBook-Air.local>
2015-09-09 20:44:37 +0800

    fixes

--- resources/views/partials/frontend/menu.blade.php
+++ resources/views/partials/frontend/menu.blade.php
@@ -26,7 +26,7 @@
     			<li><a class="@if(Request::url() === url('/research')) active @endif hide " href="{{ url('/research') }}">{{ (Session::get('locale') == 'en') ? 'RESEARCH' : '品牌洞察' }}</a></li>
                 <li><a class="@if(Request::url() === url('/process')) active @endif " href="{{ url('/process') }}">{{ (Session::get('locale') == 'en') ? 'LET\'S WORK TOGETHER' : '合作流程' }}</a></li>
                 <li><a class="@if(Request::url() === url('/contact-us')) active @endif " href="{{ url('/contact-us') }}">{{ (Session::get('locale') == 'en') ? 'CONTACT US' : '联络我们' }}</a></li>
-                <li><a href="{{ url('locale/en') }}">EN</a> | <a href="{{ url('locale/cn') }}">中文</a></li>
+                <li class="locale"><a href="{{ url('locale/en') }}">EN</a> | <a href="{{ url('locale/cn') }}">中文</a></li>
     		</ul> 
             
             <div class="social-block">   
