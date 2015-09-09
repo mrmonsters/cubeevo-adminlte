2d50c6c28afe998a5c48d04ffde5b69f5399cc22
Yc Ang <ycang@ozuraworld.com>
2015-09-09 17:24:30 +0800

    changes.

--- resources/views/management/category/index.blade.php
+++ resources/views/management/category/index.blade.php
@@ -49,7 +49,7 @@ Description for category management
 							</td>
 							<td>
 								<a href="{{ url('admin/manage/category/edit/' . $category->id) }}" class="btn btn-default">Edit</a>
-								<a onclick="javascript: if (confirm('Are you sure you want to delete this?')) { href='{{ url('admin/manage/category/destroy/' . $category->id) }}'} else { alert('Delete Cancelled.') }; " href="{{ url('admin/manage/category/destroy/' . $category->id) }}" class="btn btn-danger">Delete</a>
+								<a onclick="javascript: if (confirm('Are you sure you want to delete this?')) { href='{{ url('admin/manage/category/destroy/' . $category->id) }}'} else { alert('Delete Cancelled.') }; " href="#" class="btn btn-danger">Delete</a>
 							</td>
 						</tr>
 						@endforeach
