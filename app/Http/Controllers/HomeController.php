f01e3dc68e527a5e9044d5f6529dd6a45c2cd190
ycang89 <ycang@Ycs-MacBook-Air.local>
2015-09-16 17:54:53 +0800

    fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -195,7 +195,7 @@ class HomeController extends Controller {
 				{
 					$result = mail($email->value, 'Cubeevo Enquiry', $content,$emailHeader);
 					$response['code'] = Status::SUCCESS;
-					$response['msg']  = "Thank You for contacting us.<br/>Your message has been sent, we'll reply you within 2 working days";
+					$response['msg']  = "Thank You for contacting us.Your message has been sent, we'll reply you within 2 working days";
 				}
 			}
 		}
