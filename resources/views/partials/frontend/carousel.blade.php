5b10786064c7a192aa9b64c57941d6f6fdd71233
Yc Ang <ycang@ozuraworld.com>
2015-09-18 10:21:10 +0800

    test

--- resources/views/partials/frontend/carousel.blade.php
+++ resources/views/partials/frontend/carousel.blade.php
@@ -8,8 +8,31 @@ $imgIds = explode(",", $block->value);
         <div class="slick-carousel"> 
             <?php $count = 0; ?>
             @foreach ($imgIds as $id)
-            <div class="item {{ ($count++ == 0) ? 'active' : '' }}" style="max-height: 980px;">
+            <div class="item {{ ($count++ == 0) ? 'active' : '' }}" style="max-height: 980px;position:relative;">
                 <img data-lazy="{{ Files::find($id)->dir }}" width="100%"/> 
+                <div class="loader" style="position: absolute;
+                    top: 50%;
+                    left: 50%;
+                    width: 60px;
+                    height: 4px; 
+                    margin-left: -30px;
+                    margin-top: -2px;
+                    animation: spin 1.5s infinite linear;
+                    animation-timing-function: ease;
+                    -webkit-animation: spin 1.5s infinite linear;
+                    -webkit-animation-timing-function: ease;">
+                    <div class="loader">
+                      <div class="square" ></div>
+                      <div class="square"></div>
+                      <div class="square last"></div>
+                      <div class="square clear"></div>
+                      <div class="square"></div>
+                      <div class="square last"></div>
+                      <div class="square clear"></div>
+                      <div class="square "></div>
+                      <div class="square last"></div>
+                    </div> 
+                </div> 
             </div>
             @endforeach
         </div> 
