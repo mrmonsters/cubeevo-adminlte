6dceb6a1c104063d63a9f8d9e361ab732f822ea6
ycang <yeowchuan89@gmail.com>
2016-01-31 16:26:53 +0800

    lossless compress.

--- resources/views/partials/frontend/scripts.blade.php
+++ resources/views/partials/frontend/scripts.blade.php
@@ -1,6 +1,6 @@
 <script src="{{ asset('js/jquery-2.1.4.js') }}"></script>
 <script src="{{ asset('js/bootstrap.min.js') }}"></script> 
-<script src="{{ asset('js/jquery.fullPage.js') }}"></script> 
+<script src="{{ asset('js/jquery.fullPage.min.js') }}"></script> 
 <script src="{{ asset('js/nav.js') }}"></script> 
 <script src="{{ asset('js/jquery.parallax.js') }}"></script> 
 <script src="{{ asset('js/slick.min.js') }}"></script> 
