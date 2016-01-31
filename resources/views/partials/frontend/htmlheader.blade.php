a3480e2ef501b16b17c9ab7409036e159820d619
ycang <yeowchuan89@gmail.com>
2016-01-31 16:32:52 +0800

    adjustment.

--- resources/views/partials/frontend/htmlheader.blade.php
+++ resources/views/partials/frontend/htmlheader.blade.php
@@ -18,30 +18,7 @@ use App\Models\Files;
   <meta property="og:site_name" content="{{ $meta_title }}"/>
   <meta property="og:image" content="{{ ($id = $site_settings->where('code', 'meta_img_id')->first()->value) ? Files::find($id)->dir : '' }}" />
   <meta property="og:description" content="{{ $meta_desc }}" />
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('img/favicon.png') }}" rel="shortcut icon">
-    <!--<link rel="stylesheet" href="css/reset.css"> --> 
-    <link rel="stylesheet" href="{{ asset('css/loader.css') }}"> 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">  
-    @if(Session::get('locale') == 'en') 
-    <style type="text/css">
-      body{
-        font-family: "Conv_DINNextLTPro-Light", sans-serif;
-      } 
-      b{
-        font-family: "Conv_DINNextLTPro-Bold", sans-serif;
-      }
-    </style>
-    @endif 
-    <link rel="stylesheet" href="{{ asset('css/animate.css') }}"> 
-    <link rel="stylesheet" href="{{ asset('css/jquery.fullPage.css') }}"> 
-    <link rel="stylesheet" href="{{ asset('css/perspectiveRules.css') }}">  
-    <link rel="stylesheet" href="{{ asset('css/slick.css') }}">  
-    <link rel="stylesheet" href="{{ asset('css/slick-theme.css') }}">  
-    <link rel="stylesheet" href="{{ asset('css/custom.css') }}?v=4"> <!-- Custom style --> 
-
+     
     <script src="{{ asset('js/modernizr.js') }}"></script> <!-- Modernizr -->
     <title>{{ $meta_title }}</title>
     <?php use App\Models\Setting; ?>
