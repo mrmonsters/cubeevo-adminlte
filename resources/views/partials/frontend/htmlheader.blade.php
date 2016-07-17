a9ea0c5b7225d48e708346cd23b76934ccd45468
ycang <yeowchuan89@gmail.com>
2016-07-18 01:05:59 +0800

    adjustment.

--- resources/views/partials/frontend/htmlheader.blade.php
+++ resources/views/partials/frontend/htmlheader.blade.php
@@ -43,7 +43,7 @@ use App\Models\Files;
     <link rel="stylesheet" href="{{ asset('css/slick.css') }}">
     <link rel="stylesheet" href="{{ asset('css/slick-theme.css') }}">
     <link rel="stylesheet" href="{{ asset('css/timeline.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/custom.css') }}?v=4"> <!-- Custom style -->
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}?v=5"> <!-- Custom style -->
 
     <script async src="{{ asset('js/modernizr.js') }}"></script> <!-- Modernizr -->
     <title>{{ $meta_title }}</title>
