da11a01e7322c145c060433543de5b6634b8f57f
ycang <yeowchuan89@gmail.com>
2016-08-24 01:31:47 +0800

    adjustment.

--- resources/views/frontend/careers/employment/listing.blade.php
+++ resources/views/frontend/careers/employment/listing.blade.php
@@ -1,8 +1,8 @@
-<div class="col-xs-12">
+<div class="col-xs-12 jobs">
     @if ($jobs->count() > 0)
         <div class="row">
             @foreach ($jobs as $index => $job)
-                <div class="col-sm-push-2 col-md-5 col-md-push-1 col-lg-6 col-lg-push-1 m-b-md col-xs-push-1 col-xs-10 col-sm-8">
+                <div class="col-sm-push-2 col-md-5 col-md-push-1 col-lg-6 col-lg-push-1 m-b-md col-xs-push-1 col-xs-10 col-sm-8 job">
                     <h4>{{ strtoupper($job->title) }} </h4>
                     <?php echo html_entity_decode($job->qualification); ?>
                     <div class="m-t-lg">
