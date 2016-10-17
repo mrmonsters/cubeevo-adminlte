5ca9bf654e2e29b32e6d590235264bf55bb4a50a
ycang <yeowchuan89@gmail.com>
2016-10-17 12:20:05 +0800

    update meta.

--- database/seeds/SettingTableSeeder_2016_10_16.php
+++ database/seeds/SettingTableSeeder_2016_10_16.php
@@ -17,7 +17,7 @@ class SettingTableSeeder_2016_10_16 extends Seeder {
                 'name'  => 'Singapore Homepage Title',
                 'code'  => 'sub_homepage_title',
                 'type'  => \App\Models\Setting::SITE,
-                'value' => 'Advertising Agency | Creative & Branding Agency In Singapore & Malaysia',
+                'value' => 'Advertising Agencies Singapore | Creative Agency Singapore | Design Agency Singapore',
             ),
             array(
                 'name'  => 'Singapore Homepage Featured Project',
@@ -29,13 +29,13 @@ class SettingTableSeeder_2016_10_16 extends Seeder {
                 'name'  => 'Singapore Homepage Meta Keyword',
                 'code'  => 'sub_homepage_meta_keyword',
                 'type'  => \App\Models\Setting::SITE,
-                'value' => 'Advertising Agency | Creative & Branding Agency In Singapore & Malaysia',
+                'value' => 'Advertising Agency Singapore, Design Agency Singapore, Creative Agency Singapore, Advertising Company Singapore, Branding Agency Singapore, Graphic Design Agency Singapore',
             ),
             array(
                 'name'  => 'Singapore Homepage Meta Description',
                 'code'  => 'sub_homepage_meta_desc',
                 'type'  => \App\Models\Setting::SITE,
-                'value' => 'Advertising Agency | Creative & Branding Agency In Singapore & Malaysia',
+                'value' => 'We\'re CUBEevo, an Advertising Agency in Singapore - giving brands a complete transformation.  Are you ready to transform? Let\'s get started!',
             ),
         );
 
